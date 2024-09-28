a09f964eef361a4f93fcfaaf60ee278c87207686
IA-Website <anshumandora.21@gmail.com>
2024-09-28 14:22:57 +0530

    Temporary color update

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -79,7 +79,7 @@ const SignUpPage = () => {
 			<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
 				<p className='text-sm text-gray-400'>
 					Already have an account?{" "}
-					<Link to={"/login"} className='text-green-400 hover:underline'>
+					<Link to={"/login"} className='text-cyan-400 hover:underline'>
 						Login
 					</Link>
 				</p>
