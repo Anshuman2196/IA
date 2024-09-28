d9533050a53b464667de65a6dbbab1b6cd3dad92
IA-Website <anshumandora.21@gmail.com>
2024-09-28 13:42:14 +0530

    Temporary color update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 	return (
 		<div
 			className='min-h-screen bg-gradient-to-br
-    from-gray-900 via-cyan-900 to-blue-900 flex items-center justify-center relative overflow-hidden'
+    from-gray-900 via-cyan-900 to-skyblue-900 flex items-center justify-center relative overflow-hidden'
 		>
 			<FloatingShape color='bg-blue-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
 			<FloatingShape color='bg-indigo-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
